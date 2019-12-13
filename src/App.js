ddff02722d5366b538011785637717f9e32f2629
DenisLinenko <Denis.Linenko@outlook.com>
2019-12-13 14:18:45 -0500

    Start
    Created app
    Added bootstrap
    Created homepage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, Card, InputGroup, FormControl }  from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display: "flex", 
+      justifyContent: "center", 
+      margin: "25px",
+      alignItems: "center"}}>
+      <Card style={{ width: '100%', height: "100%" }}>
+        <Card.Body style=
+          {{
+            display: "flex", 
+            justifyContent: "center", 
+            margin: "25px",
+            alignItems: "center"
+          }}>
+          <InputGroup className="mb-3" style={{ width: '50%' }} >
+            <FormControl
+              placeholder="Search giphy"
+              aria-label="Search giphy"
+              aria-describedby="basic-addon2"
+            />
+            <InputGroup.Append>
+              <Button variant="outline-secondary">Start</Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Card.Body>
+      </Card>
     </div>
-  );
+    
+  )
 }
 
 export default App;
